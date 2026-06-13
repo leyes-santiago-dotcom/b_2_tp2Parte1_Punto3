5740d1780fa7413227ea061453064c4c1b752e8f
leyes-santiago-dotcom <leyessantifacultad@gmail.com>
2026-06-13 13:42:47 -0300

    Primer Prueba

--- b_2_tp2Parte1_Punto3/Form1.vb
+++ b_2_tp2Parte1_Punto3/Form1.vb
@@ -1,8 +1,9 @@
 ﻿Public Class Form1
     '2. Escribir un algoritmo que muestre los números pares desde 2 hasta el 100 inclusive.
-
-
-
-
-
+    Dim Pares As Integer
+    Private Sub cmdEjecutar_Click(sender As Object, e As EventArgs) Handles cmdEjecutar.Click
+        For Pares = 2 To 100 Step 2
+            lblMuestra.Text = "los pares entre 2-100 son: " & Pares
+        Next
+    End Sub
 End Class
